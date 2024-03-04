917bfbea127b363ffb45814c4d6d8dcc5a4a471c
haim raich <123101203+Haim098@users.noreply.github.com>
2024-03-04 21:41:20 +0200

    update

--- First_chack.c
+++ First_chack.c
@@ -3,30 +3,106 @@
 #include <string.h>
 #include <ctype.h>
 
-FILE* pre_assmbler(FILE* input){
-    char lint[80];//TODO: define a constant
+
+typedef struct
+{
+    char name[100];  // TODO: fix the number in array 1
+    char value[100]; // TODO: fix the number in array 2
+} macros;
+macros macro[100]; // TODO: fix the number in array 4
+enum
+{
+    true = 1,
+    false = 0
+};
+FILE *pre_assmbler(FILE *input)
+{
+
+    char lint[80];      // TODO: define a constant
+    char mcr[100][100]; // TODO: fix the number in array 3
     char *line_p;
-    FILE* f_pre_ass= fopen("pre_assmbler.am", "w");
-      if (f_pre_ass == NULL)
-        {
-            printf("error: cannot create pre_assmbler file\n");
-            exit(1);
-        }
-    while(fgets(lint, 80, input) != NULL)
+    int flag = false;
+    int i = 0;
+    char ch;
+    const int MAX_MACROS = 100; // Define constant for the maximum number of macros
+    FILE *f_pre_ass = fopen("pre_assmbler.am", "w");
+    if (f_pre_ass == NULL)
     {
+        printf("error: cannot create pre_assmbler file\n");
+        exit(1);
+    }
+    while (fgets(lint, 80, input) != NULL)
+    {
+                        
+        int j = 0;
         line_p = lint;
-        while(isspace(*line_p)){//TODO chack later this function is valid
+        while (j < i) // Adjust loop condition to iterate over the correct range of macro elements
+        {
+            if (strncmp(macro[j].name, line_p, strlen(macro[j].name)) == 0)
+            {
+                                
+                line_p = macro[j].value;
+                break;
+            }
+            j++;
+        }
+
+        if (flag==true)
+        {
+           
+          
+            if (strncmp(line_p, "endmcr", 6) == 0)
+            {
+                printf(" im here");
+                flag = false;
+                i++;
+                continue;
+            }
+            strcpy(macro[i].value, line_p);
+           /* line_p += strlen(line_p); // Move the pointer to the end of the line
+            if (*line_p != '\0') 
+            {
+                line_p++; // Move the pointer to the next line
+            }*/
+            continue;
+        }
+
+        while (isspace(*line_p))
+        {
+              
             line_p++;
         }
-        if(*line_p == '\0' || *line_p == ';'){
+        if (*line_p == '\0' || *line_p == ';')
+        {
+              
             continue;
-        }    
+        }
+        if (*line_p == 'm' && *(line_p + 1) == 'c' && *(line_p + 2) == 'r')
+        {
+            flag = true;
+            line_p += 3;
+            strcpy(macro[i].name, line_p);
+            continue;
+        }
+
         fprintf(f_pre_ass, "%s", line_p);
-        printf("%s",line_p);
+      
+     
     }
-    
+
+fclose(f_pre_ass);
+f_pre_ass = fopen("pre_assmbler.am", "r");
+if (f_pre_ass == NULL)
+{
+    printf("error: cannot open pre_assmbler file\n");
+    exit(1);
+}
+    while ((ch=fgetc(f_pre_ass))!=EOF)//TODO to delte this print
+    {
+       
+        printf("%c",ch);
+    }
     fclose(f_pre_ass);
+
     return f_pre_ass;
-   
-        
 }
